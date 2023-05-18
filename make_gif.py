e0d3d045ff6baf8d0c9e15cd7f9a98c7330c4396
Gamze Sezgin <gamzesezgin001@gmail.com>
2023-05-18 15:13:57 +0300

    make_gif.py eklendi

---
+++ make_gif.py
@@ -0,0 +1,13 @@
+import glob
+from PIL import Image
+
+
+def make_gif(frame_folder):
+    frames = [Image.open(pic) for pic in glob.glob(f"{frame_folder}/*.jpeg")]
+    frame_one = frames[0]
+    frames[0].save("download.gif", format="GIF", append_images = frames[1:], save_all = True, duration=400, loop=0)
+
+
+if __name__ == "__main__":
+    print("inside main")
+    make_gif("C:/Users/PC/PycharmProjects/CatGif/pic")
